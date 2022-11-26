94fdc46c0b7a3a3aa6a43e603c07be3554643c3c
Mohamed Sallam <59128458+mohamed-sallam@users.noreply.github.com>
2022-11-27 00:29:01 +0200

    backend/feature/4/add_group_class: edit some doc&code style stuff

--- app/src/main/java/io/github/mohamed/sallam/awb/Group.java
+++ app/src/main/java/io/github/mohamed/sallam/awb/Group.java
@@ -2,15 +2,12 @@ package io.github.mohamed.sallam.awb;
 import java.util.ArrayList;
 import java.util.UUID;
 
-	/**
-	 * This class contains the groups of apps which the user wants to block them
-	 * @author Mohamed Sherif
-	 */
-
+/**
+ * This class contains the groups of apps which the user wants to block them
+ * @author Mohamed Sherif
+ */
 public class Group {
-
 	// Fields
-
 	private ArrayList<String> softwares;
 	private ArrayList<String> websites;
 	private String name;
@@ -19,28 +16,29 @@ public class Group {
 	// Constructor
 	public Group () { }
 
+	
 	// Methods
 
 	// Mutator Methods
 	/**
-	 * Adds software
-	 * @param software the new blocked software
+	 * Adds a software to the blocked softwares list.
+	 * @param software the new blocked software.
 	 */
 	public void addSoftware(String software) {
 		this.softwares.add(software);
 	}
 
 	/**
-	 * Adds website
-	 * @param website the new blocked website
+	 * Adds a website to the blocked websites list.
+	 * @param website the new blocked website.
 	 */
 	public void addWebsite(String website) {
 		this.websites.add(website);
 	}
 
 	/**
-	 * Sets the value of uuid
-	 * @param newVar the value of uuid
+	 * Sets the value of UUID.
+	 * @param newVar the value of UUID.
 	 */
 	public void setUuid(UUID newVar) {
 		this.uuid = newVar;
@@ -49,40 +47,40 @@ public class Group {
 	// Accessor Methods
 
 	/**
-	 * Gets the value of softwares
-	 * @return array of the blocked softwares
+	 * Gets the value of softwares.
+	 * @return array of the blocked softwares.
 	 */
 	public ArrayList<String> getSoftwares() {
 		return this.softwares;
 	}
 
 	/**
-	 * Gets the value of websites
-	 * @return array of the blocked websites
+	 * Gets the value of websites.
+	 * @return array of the blocked websites.
 	 */
 	public ArrayList<String> getWebsites() {
 		return this.websites;
 	}
 
 	/**
-	 * Renames the group
-	 * @param newName the new value of group name
+	 * Renames the group.
+	 * @param newName the new value of group name.
 	 */
 	public void rename(String newName) {
 		this.name = newName;
 	}
 
 	/**
-	 * Gets group name
-	 * @return the value of name
+	 * Gets group name.
+	 * @return the value of name.
 	 */
 	public String getName() {
 		return this.name;
 	}
 
 	/**
-	 * Gets the value of uuid
-	 * @return the value of uuid
+	 * Gets the value of UUID.
+	 * @return the value of UUID.
 	 */
 	public UUID getUuid() {
 		return this.uuid;
@@ -91,29 +89,29 @@ public class Group {
 	// Other Methods
 
 	/**
-	 * Makes a copy of the current group
-	 * @return newGroup
+	 * Makes a copy of the current group.
+	 * @return A copy of `Group` object.
 	 */
 	public Group clone() {
 		Group newGroup = new Group();
-		newGroup.softwares = this.getSoftwares();
-		newGroup.websites = this.getWebsites();
-		newGroup.setUuid(this.uuid);
-		newGroup.rename(this.name);
+		newGroup.softwares = this.softwares;
+		newGroup.websites  = this.websites;
+		newGroup.uuid      = this.uuid;
+		newGroup.name      = this.name;
 		return newGroup;
 	}
 
 	/**
-	 * Deletes Software
-	 * @param index of the software desired to be deleted
+	 * Deletes a software from blocked softwares list.
+	 * @param index of the software desired to be deleted.
 	 */
 	public void deleteSoftware(int index) {
 		this.softwares.remove(index);
 	}
 
 	/**
-	 * Deletes Website
-	 * @param index of the website desired to be deleted
+	 * Deletes website from blocked websites list.
+	 * @param index of the website desired to be deleted.
 	 */
 	public void deleteWebsite(int index) {
 		this.websites.remove(index);
