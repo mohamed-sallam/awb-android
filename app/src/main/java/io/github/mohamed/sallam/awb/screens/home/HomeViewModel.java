73f173bd751c737f23b93d7d557bde97fe88f9b1
Mohamed Sallam <59128458+mohamed-sallam@users.noreply.github.com>
2022-11-29 00:03:33 +0200

    frontend/feat/21/setup_viewmodel_n_databinding: add viewmodel

---
+++ app/src/main/java/io/github/mohamed/sallam/awb/screens/home/HomeViewModel.java
@@ -0,0 +1,21 @@
+package io.github.mohamed.sallam.awb.screens.home;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+import java.util.List;
+
+import io.github.mohamed.sallam.awb.Group;
+
+public class HomeViewModel extends ViewModel {
+    private MutableLiveData<List<Group>> groups;
+
+    public MutableLiveData<List<Group>> getGroups() {
+        return groups;
+    }
+
+    public void setGroups(MutableLiveData<List<Group>> groups) {
+        this.groups = groups;
+    }
+}
