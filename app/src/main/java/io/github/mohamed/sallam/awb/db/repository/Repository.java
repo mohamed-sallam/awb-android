e090e8b332e35d35d523999901c2460a3fe105e1
Abdalrhman Hamida <abdalrhman7mida@gmail.com>
2022-12-02 22:54:03 +0200

    backend/feat/35/create_repo: add declaration DAOs func
    -[x] Add declaration for all DAOs functions

--- app/src/main/java/io/github/mohamed/sallam/awb/db/repository/Repository.java
+++ app/src/main/java/io/github/mohamed/sallam/awb/db/repository/Repository.java
@@ -2,12 +2,28 @@ package io.github.mohamed.sallam.awb.db.repository;
 
 import android.app.Application;
 
+import androidx.lifecycle.LiveData;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
 import androidx.room.RoomDatabase;
+import androidx.room.Transaction;
+import androidx.room.Update;
+
+import java.util.List;
+import java.util.UUID;
 
 import io.github.mohamed.sallam.awb.db.dao.BlockedAppDao;
 import io.github.mohamed.sallam.awb.db.dao.DetoxPeriodDao;
 import io.github.mohamed.sallam.awb.db.dao.DeviceDao;
 import io.github.mohamed.sallam.awb.db.dao.GroupDao;
+import io.github.mohamed.sallam.awb.db.entity.BlockedApp;
+import io.github.mohamed.sallam.awb.db.entity.DetoxPeriod;
+import io.github.mohamed.sallam.awb.db.entity.Device;
+import io.github.mohamed.sallam.awb.db.entity.Group;
+import io.github.mohamed.sallam.awb.db.relationship.DetoxPeriodAndGroup;
+import io.github.mohamed.sallam.awb.db.relationship.DeviceWithGroups;
+import io.github.mohamed.sallam.awb.db.relationship.GroupWithBlockedApps;
 
 public class Repository {
     BlockedAppDao blockedAppDao;
@@ -15,12 +31,97 @@ public class Repository {
     DeviceDao deviceDao;
     GroupDao groupDao;
 
-    public Repository(Application application){
+    public Repository(Application application) {
         UserDatabase db = UserDatabase.getDatabase(application);
         blockedAppDao = db.blockedAppDao();
         detoxPeriodDao = db.detoxPeriodDao();
         deviceDao = db.deviceDao();
         groupDao = db.groupDao();
+    }
+
+    // BlockedAppDao
+    public void insert(BlockedApp blockedApp) {
+
+    }
+
+    public void delete(BlockedApp blockedApp) {
+
+    }
+
+    public LiveData<List<BlockedApp>> getAll() {
+
+    }
+
+    public void clone(UUID sourceGroupUuid, UUID destinationGroupUuid) {
+
+    }
+
+    // DetoxPeriodDao
+    public void insert(DetoxPeriod detoxPeriod) {
+        
+    }
+
+    public void update(DetoxPeriod detoxPeriod) {
+
+    }
+
+    public void delete(DetoxPeriod detoxPeriod) {
+
+    }
+
+    public LiveData<DetoxPeriod> get(int id) {
+
+    }
+
+    public LiveData<DetoxPeriodAndGroup> getWithGroup(int id) {
+
+    }
+
+    //DeviceDao
+
+    public void insert(Device device) {
+
+    }
+
+    public void update(Device device) {
+
+    }
+
+    public void delete(Device device) {
+
+    }
+
+    public void setUuid(UUID oldUuid, UUID newUuid) {
+
+    }
+
+    public LiveData<List<Device>> getAll() {
+
+    }
+
+    public LiveData<List<DeviceWithGroups>> getAllWithGroups() {
+
+    }
+
+    //GroupDao
+    public void insert(Group group) {
+
+    }
+
+    public void update(Group group) {
+
+    }
+
+    public void delete(Group group) {
+
+    }
+
+    public LiveData<List<Group>> getAll() {
+
+    }
+
+    public LiveData<List<GroupWithBlockedApps>> getAllWithBlockedApps() {
+
     }
 
 }
