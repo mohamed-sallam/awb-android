4027751b6c96a3f2e9eff7111d959756986d7b88
Mohamed Sallam <59128458+mohamed-sallam@users.noreply.github.com>
2022-12-02 04:34:26 +0200

    backend/feat/30/data_entities1: Finish Device entity

--- app/src/main/java/io/github/mohamed/sallam/awb/Device.java
+++ app/src/main/java/io/github/mohamed/sallam/awb/Device.java
@@ -1,6 +1,11 @@
 package io.github.mohamed.sallam.awb;
 
 import androidx.annotation.NonNull;
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+import androidx.room.TypeConverters;
+
 import static io.github.mohamed.sallam.awb.Device.Os.*;
 import java.util.ArrayList;
 import java.util.UUID;
@@ -10,16 +15,23 @@ import java.util.UUID;
  * and generate a unique id for the device.
  *
  * @author Abdalrhman Hemida
+ * @author Mohamed Sallam
  */
+@TypeConverters({UuidConverter.class})
+@Entity(tableName = "devices_table")
 public class Device {
     // Fields
-    private String name;
-    private String operatingSystemName;
-    private Os operatingSystemType = UNKOWN;
+    @NonNull
+    @PrimaryKey
+    public UUID uuid = UUID.randomUUID();
+    public String name;
+    public boolean thisDevice;
+    public String operatingSystemName;
+    public Os operatingSystemType = UNKNOWN;
+    @ColumnInfo(defaultValue = "127.0.0.1")
+    public String ipAddressV4;
+    public String secretKey;
     public static final String AWB_VERSION = "0.1.0v";
-    private String ipAddressV4 = "127.0.0.1";
-    private UUID uuid;
-    private ArrayList<Group> groups = new ArrayList<>();
 
     /**
      * Enum to optimize the interactions with operating system names by
@@ -29,7 +41,7 @@ public class Device {
      * @author Abdalrhman Hemida
      */
     public enum Os {
-        UNKOWN(0),
+        UNKNOWN(0),
         UNIX(1),
         ANDROID(2),
         WINDOWS(3);
@@ -45,131 +57,5 @@ public class Device {
         public String toString() {
             return Integer.toString(osNum);
         }
-    }
-
-    // Constructor
-    public Device() {}
-
-    // Mutators
-    /**
-     * Sets the value of device name.
-     *
-     * @param newVar the new value of device name.
-     */
-    public void setName(String newVar) {
-        name = newVar;
-    }
-
-    /**
-     * Sets the value of operatingSystemName.
-     *
-     * @param newVar the new value of operatingSystemName.
-     *
-     */
-    public void setOperatingSystemName(String newVar) {
-        operatingSystemName = newVar;
-    }
-
-    /**
-     * Sets the value of operating system type.
-     *
-     * @param newVar the new value of operatingSystemType.
-     */
-    public void setOperatingSystemType(Os newVar) {
-        operatingSystemType = newVar;
-    }
-
-    /**
-     * Sets the value of device's IP Address (V4).
-     *
-     * @param newVar the new value of ipAddressV4.
-     */
-    public void setIpAddressV4(String newVar) {
-        ipAddressV4 = newVar;
-    }
-
-    /**
-     * Generates new UUID for a device the user has added.
-     * UUID is a unique id to identify devices.
-     */
-    public void generateUuid() {}
-
-    // Accessors
-    /**
-     * Gets the device name.
-     *
-     * @return the value of name.
-     */
-    public String getName() {
-        return name;
-    }
-
-    /**
-     * Gets the operating system name.
-     *
-     * @return the value of operating system name.
-     */
-    public String getOperatingSystemName() {
-        return operatingSystemName;
-    }
-
-    /**
-     * Gets the value of IP Address (V4) of the Device.
-     *
-     * @return the value of IP Address.
-     */
-    public String getIpAddressV4() {
-        return ipAddressV4;
-    }
-
-    /**
-     * Gets the value of UUID.
-     *
-     * @return device UUID.
-     */
-    public UUID getUuid() {
-        return uuid;
-    }
-
-    /**
-     * Gets a group of blocked apps.
-     *
-     * @return ArrayList of detoxing apps groups.
-     */
-    public ArrayList<Group> getGroups() {
-        return groups;
-    }
-
-    /**
-     * Gets the value of operating system type.
-     *
-     * @return the value of operating system type.
-     */
-    public Os getOperatingSystemType() {
-        return operatingSystemType;
-    }
-
-    // Methods
-    /**
-     * Deletes a group of apps which user has defined from a particular device
-     * using the UUID of the group.
-     *
-     * @param groupUuid the group id.
-     *
-     * @author Abdalrhman Hemida.
-     */
-    public void deleteGroup(UUID groupUuid) {
-        groups.removeIf(group -> group.getUuid().equals(groupUuid));
-    }
-
-    /**
-     * Adds a group of detoxing softwares/websites for a particular device.
-     *
-     * @param group the group of apps to be added.
-     *
-     * @author Abdalrhman Hemida.
-     */
-    public void addGroup(Group group) {
-        groups.add(group);
     }
 }
