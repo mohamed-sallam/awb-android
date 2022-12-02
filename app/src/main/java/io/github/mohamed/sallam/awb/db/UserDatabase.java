7146cd8a4a6371bc5f9fb58bf34e65d2cc78093d
Mohd-Sherif <Mohd-Sherif@outlook.com>
2022-12-03 00:14:51 +0200

    backend/feat/33/create_database: Removed 2 Entities
    - [x] Removed `DetoxSchedule.class` & `DetoxSetting.class` from the entities.

--- app/src/main/java/io/github/mohamed/sallam/awb/db/UserDatabase.java
+++ app/src/main/java/io/github/mohamed/sallam/awb/db/UserDatabase.java
@@ -15,8 +15,6 @@ import io.github.mohamed.sallam.awb.db.dao.DeviceDao;
 import io.github.mohamed.sallam.awb.db.dao.GroupDao;
 import io.github.mohamed.sallam.awb.db.entity.BlockedApp;
 import io.github.mohamed.sallam.awb.db.entity.DetoxPeriod;
-import io.github.mohamed.sallam.awb.db.entity.DetoxSchedule;
-import io.github.mohamed.sallam.awb.db.entity.DetoxSettings;
 import io.github.mohamed.sallam.awb.db.entity.Device;
 import io.github.mohamed.sallam.awb.db.entity.Group;
 
@@ -27,9 +25,8 @@ import io.github.mohamed.sallam.awb.db.entity.Group;
  *
  * @author Mohamed Sherif
  */
-@Database(entities = {BlockedApp.class, DetoxPeriod.class, DetoxSchedule.class,
-        DetoxSettings.class, Device.class, Group.class},
-        version = 1, exportSchema = false)
+@Database(entities = {BlockedApp.class, DetoxPeriod.class,
+        Device.class, Group.class}, version = 1, exportSchema = false)
 public abstract class UserDatabase extends RoomDatabase {
     public abstract BlockedAppDao blockedAppDao();
     public abstract DetoxPeriodDao detoxPeriodDao();
