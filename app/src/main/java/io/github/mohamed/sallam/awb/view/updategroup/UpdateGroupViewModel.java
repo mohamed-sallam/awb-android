1ed6cc221c65dc58b7315b9b3bb0b0e2d17ce9df
MohamedYehia20m <medoyehia2001@gmail.com>
2022-12-08 22:33:24 +0200

    backend/feat/38/create_update_group_view_model
    - [x] create `UpdateGroupViewModel`

--- app/src/main/java/io/github/mohamed/sallam/awb/view/updategroup/UpdateGroupViewModel.java
+++ app/src/main/java/io/github/mohamed/sallam/awb/view/updategroup/UpdateGroupViewModel.java
@@ -1,18 +1,58 @@
 package io.github.mohamed.sallam.awb.view.updategroup;
 
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
 import androidx.lifecycle.MutableLiveData;
-import androidx.lifecycle.ViewModel;
 
 import java.util.List;
 
-public class UpdateGroupViewModel extends ViewModel {
-    private MutableLiveData<List<String>> app;
+public class UpdateGroupViewModel extends AndroidViewModel{
 
-    public MutableLiveData<List<String>> getApp() {
-        return app;
+    // GroupRepository constructor
+    GroupRepository grouprepository;
+    public UpdateGroupViewModel(@NonNull Application application) {
+        super(application);
+        grouprepository = new GroupRepository(application);
     }
 
-    public void setApp(MutableLiveData<List<String>> app) {
-        this.app = app;
+    //GroupRepository methods
+    public void insert(Group group) {
+        grouprepository.insert(group);
+    }
+
+    public void rename(String name, UUID uuid) {
+        grouprepository.rename(name, uuid);
+    }
+
+    public void delete(UUID groupUuid) {
+        grouprepository.delete(groupUuid);
+    }
+
+    public LiveData<List<Group>> getAll(UUID deviceUuid) {
+        return grouprepository.getAll(deviceUuid);
+    }
+
+    public LiveData<List<GroupWithBlockedApps>>
+    getAllWithBlockedApps(UUID deviceUuid) {
+        return grouprepository.getAllWithBlockedApps(deviceUuid);
+    }
+
+    public void insertBlockedApp(BlockedApp blockedApp) {
+        grouprepository.insertBlockedApp(blockedApp);
+    }
+
+    public void deleteBlockedApp(BlockedApp blockedApp) {
+        grouprepository.deleteBlockedApp(blockedApp);
+    }
+
+    public LiveData<List<BlockedApp>>
+    getAllBlockedApps(UUID deviceUuid, UUID groupUuid) {
+        return grouprepository.getAllBlockedApps(deviceUuid,groupUuid);
+    }
+
+    public void clone (UUID sourceGroupUuid, String groupName) {
+        grouprepository.clone(sourceGroupUuid,groupName);
     }
 }
