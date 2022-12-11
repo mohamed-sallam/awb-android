13490bef4894f7959bb9fffa58972375ac369393
Mohamed Sallam <59128458+mohamed-sallam@users.noreply.github.com>
2022-12-11 03:00:13 +0200

    backend/feat/38/create_view_model: make packageName unique ∀ groupUuid
    Source: https://stackoverflow.com/a/48962768/6646715

--- app/src/main/java/io/github/mohamed/sallam/awb/db/entity/BlockedApp.java
+++ app/src/main/java/io/github/mohamed/sallam/awb/db/entity/BlockedApp.java
@@ -1,6 +1,7 @@
 package io.github.mohamed.sallam.awb.db.entity;
 
 import androidx.room.Entity;
+import androidx.room.Index;
 import androidx.room.PrimaryKey;
 import androidx.room.TypeConverters;
 
@@ -9,7 +10,15 @@ import java.util.UUID;
 import io.github.mohamed.sallam.awb.db.converter.UuidConverter;
 
 @TypeConverters({UuidConverter.class})
-@Entity(tableName = "blocked_apps_table")
+@Entity(
+        tableName = "blocked_apps_table",
+        indices = {
+                @Index(
+                        value = {"packageName", "groupUuid"},
+                        unique = true
+                )
+        }
+)
 public class BlockedApp {
     // Fields
     @PrimaryKey(autoGenerate = true)
