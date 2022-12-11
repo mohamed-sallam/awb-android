560e2f62409dd298f7dec69e7a37ca826f741805
Mohamed Sallam <59128458+mohamed-sallam@users.noreply.github.com>
2022-12-11 03:04:33 +0200

    backend/feat/38/create_view_model: Impl UpdateGroupViewModel

--- app/src/main/java/io/github/mohamed/sallam/awb/view/updategroup/UpdateGroupViewModel.java
+++ app/src/main/java/io/github/mohamed/sallam/awb/view/updategroup/UpdateGroupViewModel.java
@@ -4,55 +4,73 @@ import android.app.Application;
 
 import androidx.annotation.NonNull;
 import androidx.lifecycle.AndroidViewModel;
-import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.LiveData;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
-public class UpdateGroupViewModel extends AndroidViewModel{
+import io.github.mohamed.sallam.awb.db.entity.BlockedApp;
+import io.github.mohamed.sallam.awb.db.relationship.GroupWithBlockedApps;
+import io.github.mohamed.sallam.awb.repo.GroupRepository;
 
-    // GroupRepository constructor
-    GroupRepository grouprepository;
-    public UpdateGroupViewModel(@NonNull Application application) {
+/**
+ * UpdateGroupFragment ViewModel. It uses Command Pattern to queue applications
+ * blocking and whitelisting til user save these command by clicking <save> or
+ * discard them by clicking <cancel>.
+ * Source: refactoring.guru/design-patterns/command
+ *
+ * @author Mohamed Sallam
+ */
+public class UpdateGroupViewModel extends AndroidViewModel {
+    private final GroupRepository groupRepository;
+    private final List<AppCommand> appCommands;
+    private final UUID groupUuid;
+
+    public UpdateGroupViewModel(@NonNull Application application, UUID groupUuid) {
         super(application);
-        grouprepository = new GroupRepository(application);
+        groupRepository = new GroupRepository(application);
+        appCommands = new ArrayList<>();
+        this.groupUuid = groupUuid;
     }
 
-    //GroupRepository methods
-    public void insert(Group group) {
-        grouprepository.insert(group);
+    public LiveData<GroupWithBlockedApps> getGroupWithBlockedApps() {
+        return groupRepository.getWithBlockedApps(groupUuid);
     }
 
-    public void rename(String name, UUID uuid) {
-        grouprepository.rename(name, uuid);
+    public void allowApp(String packageName) {
+        appCommands.add(new AppCommand(packageName) {
+            @Override
+            public void execute() {
+                BlockedApp blockedApp = new BlockedApp();
+                blockedApp.groupUuid = groupUuid;
+                blockedApp.packageName = packageName;
+                groupRepository.insertBlockedApp(blockedApp);
+            }
+        });
     }
 
-    public void delete(UUID groupUuid) {
-        grouprepository.delete(groupUuid);
+    public void blockApp(String packageName) {
+        appCommands.add(new AppCommand(packageName) {
+            @Override
+            void execute() {
+                groupRepository.deleteBlockedApp(groupUuid, packageName);
+            }
+        });
     }
 
-    public LiveData<List<Group>> getAll(UUID deviceUuid) {
-        return grouprepository.getAll(deviceUuid);
+    public void save() {
+        for (AppCommand appCommand: appCommands)
+            appCommand.execute();
     }
 
-    public LiveData<List<GroupWithBlockedApps>>
-    getAllWithBlockedApps(UUID deviceUuid) {
-        return grouprepository.getAllWithBlockedApps(deviceUuid);
-    }
+    private abstract class AppCommand {
+        protected String packageName;
 
-    public void insertBlockedApp(BlockedApp blockedApp) {
-        grouprepository.insertBlockedApp(blockedApp);
-    }
+        public AppCommand(String packageName) {
+            this.packageName = packageName;
+        }
 
-    public void deleteBlockedApp(BlockedApp blockedApp) {
-        grouprepository.deleteBlockedApp(blockedApp);
-    }
-
-    public LiveData<List<BlockedApp>>
-    getAllBlockedApps(UUID deviceUuid, UUID groupUuid) {
-        return grouprepository.getAllBlockedApps(deviceUuid,groupUuid);
-    }
-
-    public void clone (UUID sourceGroupUuid, String groupName) {
-        grouprepository.clone(sourceGroupUuid,groupName);
+        abstract void execute();
     }
 }
