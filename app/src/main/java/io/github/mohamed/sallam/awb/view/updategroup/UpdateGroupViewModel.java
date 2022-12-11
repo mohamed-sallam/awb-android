c6d922728e96c618e9b8401f2d79724f11078ede
Mohamed Sallam <59128458+mohamed-sallam@users.noreply.github.com>
2022-12-11 06:13:02 +0200

    backend/feat/38/create_view_model: Using linkedHashMap to prevent repetition
    This way preventing long list of blocking and unblocking of the same app many times. By this way, we only care about the conclusion of the operation on one package name.

--- app/src/main/java/io/github/mohamed/sallam/awb/view/updategroup/UpdateGroupViewModel.java
+++ app/src/main/java/io/github/mohamed/sallam/awb/view/updategroup/UpdateGroupViewModel.java
@@ -6,8 +6,9 @@ import androidx.annotation.NonNull;
 import androidx.lifecycle.AndroidViewModel;
 import androidx.lifecycle.LiveData;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Objects;
 import java.util.UUID;
 
 import io.github.mohamed.sallam.awb.db.entity.BlockedApp;
@@ -24,13 +25,13 @@ import io.github.mohamed.sallam.awb.repo.GroupRepository;
  */
 public class UpdateGroupViewModel extends AndroidViewModel {
     private final GroupRepository groupRepository;
-    private final List<AppCommand> appCommands;
+    private final Map<String, AppCommand> appCommands;
     private final UUID groupUuid;
 
     public UpdateGroupViewModel(@NonNull Application application, UUID groupUuid) {
         super(application);
         groupRepository = new GroupRepository(application);
-        appCommands = new ArrayList<>();
+        appCommands = new LinkedHashMap<>();
         this.groupUuid = groupUuid;
     }
 
@@ -39,36 +40,50 @@ public class UpdateGroupViewModel extends AndroidViewModel {
     }
 
     public void allowApp(String packageName) {
-        appCommands.add(new AppCommand(packageName) {
-            @Override
-            public void execute() {
-                BlockedApp blockedApp = new BlockedApp();
-                blockedApp.groupUuid = groupUuid;
-                blockedApp.packageName = packageName;
-                groupRepository.insertBlockedApp(blockedApp);
-            }
-        });
+        AppCommand command = appCommands.get(packageName);
+        if (command == null) {
+            appCommands.put(packageName, new AppCommand(packageName, true) {
+                @Override
+                public void execute() {
+                    BlockedApp blockedApp = new BlockedApp();
+                    blockedApp.groupUuid = groupUuid;
+                    blockedApp.packageName = packageName;
+                    groupRepository.insertBlockedApp(blockedApp);
+                }
+            });
+            return;
+        }
+        if (!command.isAllowCommand)
+            appCommands.remove(packageName);
     }
 
     public void blockApp(String packageName) {
-        appCommands.add(new AppCommand(packageName) {
-            @Override
-            void execute() {
-                groupRepository.deleteBlockedApp(groupUuid, packageName);
-            }
-        });
+        AppCommand command = appCommands.get(packageName);
+        if (command == null) {
+            appCommands.put(packageName, new AppCommand(packageName, false) {
+                @Override
+                void execute() {
+                    groupRepository.deleteBlockedApp(groupUuid, packageName);
+                }
+            });
+            return;
+        }
+        if (command.isAllowCommand)
+            appCommands.remove(packageName);
     }
 
     public void save() {
-        for (AppCommand appCommand: appCommands)
+        for (AppCommand appCommand: appCommands.values())
             appCommand.execute();
     }
 
     private abstract class AppCommand {
         protected String packageName;
+        protected Boolean isAllowCommand;
 
-        public AppCommand(String packageName) {
+        public AppCommand(String packageName, Boolean isAllowCommand) {
             this.packageName = packageName;
+            this.isAllowCommand = isAllowCommand;
         }
 
         abstract void execute();
