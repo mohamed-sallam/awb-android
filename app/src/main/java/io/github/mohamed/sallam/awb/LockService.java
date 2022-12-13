6c2a2afb32bfd18860650f43f2115a578123c4f7
Mohd-Sherif <Mohd-Sherif@outlook.com>
2022-12-13 12:55:55 +0200

    backend/feat/40/lock_behaviour: Add Notification n `startForegroundService`

--- app/src/main/java/io/github/mohamed/sallam/awb/LockService.java
+++ app/src/main/java/io/github/mohamed/sallam/awb/LockService.java
@@ -1,13 +1,23 @@
 package io.github.mohamed.sallam.awb;
 
+import android.app.ActivityManager;
+import android.app.Notification;
+import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
 
 import androidx.annotation.Nullable;
+import androidx.core.app.NotificationCompat;
 
+import io.github.mohamed.sallam.awb.view.mainlauncher.MainLauncherActivity;
+
+/**
+ * Lock Foreground Service Class
+ *
+ * @author Mohamed Sherif
+ */
 public class LockService extends Service {
-
     @Nullable
     @Override
     public IBinder onBind(Intent intent) {
@@ -21,7 +31,23 @@ public class LockService extends Service {
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-        startForeground();
+        Intent lockNotificationIntent = new Intent(
+                this, MainLauncherActivity.class
+        );
+        PendingIntent pendingLockNotificationIntent = PendingIntent.getActivity(
+                this, 0, lockNotificationIntent, 0
+        );
+
+        Notification lockNotification =
+                new NotificationCompat.Builder(this, "LockChannelID")
+                        .setContentTitle("Detox Period Started")
+                        .setContentText("Lock Service in operation")
+                        .setSmallIcon(R.drawable.ic_lock)
+                        .setContentIntent(pendingLockNotificationIntent)
+                        .build();
+
+        startForeground(1,lockNotification);
+
         return START_STICKY;
     }
 
