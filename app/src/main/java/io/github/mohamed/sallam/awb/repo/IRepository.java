dbd2314c93a0eb07d611d9d26f8ca5fa06844777
Abdalrhman Hamida <abdalrhman7mida@gmail.com>
2022-12-16 04:52:14 +0200

    doc/doc/45/documentation: Documentation for `IRepository`
    - [x] Documentation for `IRepository` interface

--- app/src/main/java/io/github/mohamed/sallam/awb/repo/IRepository.java
+++ app/src/main/java/io/github/mohamed/sallam/awb/repo/IRepository.java
@@ -1,4 +1,44 @@
 package io.github.mohamed.sallam.awb.repo;
+/**
+ * Repository pattern apply a design principle 'separation of concerns' which
+ * makes our classes unit testable. It applies Principle of Dependency Inversion
+ * (or code to abstractions, not concretions), To make our code more robust to
+ * changes.
+ * Repositories classes are responsible for handling and implementing operations,
+ * and adding the full logic to execute a specific operation by using DAOs methods.
+ *
+ * Using 'lamda expression' over the whole methods of repositories to optimize
+ * the code. Lambda Expressions were added in Java 8.
+ *
+ * Why using lamda expression?
+ * Makes our code more readable and shorter. Instead of instantiation an
+ * anonymous inner class that implements the abstract method run() on the
+ * interface 'Runnable', we use lamdad expression to do that.
+ *
+ * For example: instead of writing
+ * ```
+ * UserDatabase.databaseWriteExecutor.execute(new Runnable() {
+ *             @Override
+ *             public void run() {
+ *                 detoxPeriodDao.insert(detoxPeriod);
+ *             }
+ *         });
+ * ```
+ * we write:
+ * ```
+ * UserDatabase.databaseWriteExecutor.execute(
+ *                 () -> detoxPeriodDao.insert(detoxPeriod)
+ *         );
+ * ```
+ * Source: https://code.tutsplus.com/tutorials/
+ * java-8-for-android-cleaner-code-with-lambda-expressions--cms-29661
+ *
+ * Generics used to provide compile-time type checking and removing risk of
+ * `ClassCastException` that was common while working with collection classes.
+ * {@link IAggregateRoot} to define the aggregate roots to extend.
+ *
+ * @author Abdalrhman Hemida
+ */
 
 public interface IRepository<T extends IAggregateRoot> {
     void insert(T entity);
