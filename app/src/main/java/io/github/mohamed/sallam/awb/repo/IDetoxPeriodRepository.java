c2b2902c8cb18d929b2d6669d195be1f24a1c25e
Abdalrhman Hamida <abdalrhman7mida@gmail.com>
2022-12-16 04:47:26 +0200

    doc/doc/45/documentation: `IDetoxPeriodRepository` documentaion
    -[x] Documentation for `IDetoxPeriodRepository` interface.

--- app/src/main/java/io/github/mohamed/sallam/awb/repo/IDetoxPeriodRepository.java
+++ app/src/main/java/io/github/mohamed/sallam/awb/repo/IDetoxPeriodRepository.java
@@ -7,6 +7,17 @@ import io.github.mohamed.sallam.awb.db.relationship.DetoxPeriodAndGroup;
 import io.github.mohamed.sallam.awb.db.relationship.DetoxPeriodAndGroupWithWhitelistedApps;
 
 public interface IDetoxPeriodRepository extends IRepository<DetoxPeriod> {
+/**
+ * {@link IRepository}
+ *
+ * `IDetoxPeriodRepository` interface is responsible for handling and declaring
+ * detox period operations (insert - update - delete - getters). On class
+ * `DetoxPeriodRepository` we add the full logic and implementation to execute
+ * a specific operation by using DAOs methods.
+ *
+ * @author Abdalrhman Hemida
+ * @author Mohamed Yehia
+ */
     void update(DetoxPeriod detoxPeriod);
     LiveData<DetoxPeriod> get(Integer id);
     LiveData<DetoxPeriodAndGroup> getDetoxPeriodAndGroup(Integer id);
