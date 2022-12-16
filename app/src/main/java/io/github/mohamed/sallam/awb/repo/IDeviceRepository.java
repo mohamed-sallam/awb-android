542789d5a2cf11d5e4401a5a80817b10cbfe05d6
Abdalrhman Hamida <abdalrhman7mida@gmail.com>
2022-12-16 07:21:26 +0200

    doc/doc/45/documentation: methods of `IDeviceRepository`
    - [x] Documentation for update method
    - [x] Documentation for generateUuid method

--- app/src/main/java/io/github/mohamed/sallam/awb/repo/IDeviceRepository.java
+++ app/src/main/java/io/github/mohamed/sallam/awb/repo/IDeviceRepository.java
@@ -18,7 +18,19 @@ import io.github.mohamed.sallam.awb.db.relationship.DeviceWithGroups;
  * @author Mohamed Yehia
  */
 public interface IDeviceRepository extends IRepository<Device, UUID> {
+    /**
+     * Updates device in database.
+     *
+     * @param device object of the blocking period.
+     */
     void update(Device device);
+
+    /**
+     * Generates a new UUID, unique identifier for a device.
+     *
+     * @param oldUuid the old uuid for a specific device, in order to create a
+     * new UUID for it.
+     */
     void generateUuid(UUID oldUuid);
     LiveData<List<Device>> getAll();
     LiveData<List<DeviceWithGroups>> getAllWithGroups();  // TODO maybe deleted
