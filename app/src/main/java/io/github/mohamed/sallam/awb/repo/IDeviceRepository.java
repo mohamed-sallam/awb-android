568aa82b57a6fd30852c58dae1acf9521ac19a12
Abdalrhman Hamida <abdalrhman7mida@gmail.com>
2022-12-16 07:20:13 +0200

    doc/doc/45/documentation: `IDeviceRepository` interface

--- app/src/main/java/io/github/mohamed/sallam/awb/repo/IDeviceRepository.java
+++ app/src/main/java/io/github/mohamed/sallam/awb/repo/IDeviceRepository.java
@@ -6,6 +6,17 @@ import java.util.UUID;
 import io.github.mohamed.sallam.awb.db.entity.Device;
 import io.github.mohamed.sallam.awb.db.relationship.DeviceWithGroups;
 
+/**
+ * {@link IRepository}
+ *
+ * `IDeviceRepository` interface is responsible for handling and declaring
+ * device operations (insert - update - delete - generateUuid - getters). On class
+ * `DeviceRepository` we add the full logic and implementation to execute
+ * a specific operation by using DAOs methods.
+ *
+ * @author Abdalrhman Hemida
+ * @author Mohamed Yehia
+ */
 public interface IDeviceRepository extends IRepository<Device, UUID> {
     void update(Device device);
     void generateUuid(UUID oldUuid);
