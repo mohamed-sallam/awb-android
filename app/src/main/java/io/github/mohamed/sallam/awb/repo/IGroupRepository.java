7fcd0a7cc9d5d46556a39cc7fd0c5a79d76b3770
Abdalrhman Hamida <abdalrhman7mida@gmail.com>
2022-12-16 18:05:14 +0200

    doc/doc/45/documentation: doc for `IGroupRepository`
    - [x] Doc for the interface `IGroupRepository`.

--- app/src/main/java/io/github/mohamed/sallam/awb/repo/IGroupRepository.java
+++ app/src/main/java/io/github/mohamed/sallam/awb/repo/IGroupRepository.java
@@ -9,6 +9,17 @@ import io.github.mohamed.sallam.awb.db.entity.WhitelistedApp;
 import io.github.mohamed.sallam.awb.db.entity.Group;
 import io.github.mohamed.sallam.awb.db.relationship.GroupWithWhitelistedApps;
 
+/**
+ * {@link IRepository}
+ *
+ * `IGroupRepository` interface is responsible for handling and declaring
+ * group operations (insert - update - delete - rename - clone - getters).
+ * On class `GroupRepository` we add the full logic and implementation to execute
+ * a specific operation by using DAOs methods.
+ *
+ * @author Abdalrhman Hemida
+ * @author Mohamed Yehia
+ */
 public interface IGroupRepository extends IRepository<Group> {
     void rename(UUID uuid, String name);
     LiveData<List<Group>> getAllByDevice(UUID deviceUuid);
