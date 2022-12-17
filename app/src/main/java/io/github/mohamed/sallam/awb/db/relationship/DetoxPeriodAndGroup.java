e641e05babd817e852e968e041724b9655a3bead
Abdalrhman Hamida <abdalrhman7mida@gmail.com>
2022-12-17 16:48:07 +0200

    doc/doc/45/documentation: Doc `doc/doc/45/documentation: Doc  `DetoxPeriodAndGroupWithWhitelistedApps``

--- app/src/main/java/io/github/mohamed/sallam/awb/db/relationship/DetoxPeriodAndGroup.java
+++ app/src/main/java/io/github/mohamed/sallam/awb/db/relationship/DetoxPeriodAndGroup.java
@@ -6,6 +6,13 @@ import androidx.room.Relation;
 import io.github.mohamed.sallam.awb.db.entity.DetoxPeriod;
 import io.github.mohamed.sallam.awb.db.entity.Group;
 
+/**
+ * `DetoxPeriodAndGroup` class is a relationship between two entities `Group`
+ * and `DetoxPeriod` in order to query a detox period with the corresponding group
+ * from database by using DAO method. It's one-to-one relationships.
+ *
+ * Source: https://developer.android.com/training/data-storage/room/relationships
+ */
 public class DetoxPeriodAndGroup {
     @Embedded
     public DetoxPeriod detoxPeriod;
