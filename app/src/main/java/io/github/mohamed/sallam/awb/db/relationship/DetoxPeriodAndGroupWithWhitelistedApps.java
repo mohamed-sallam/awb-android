d574d835f90239f5d20a533ae8ce393b923b1bc3
Abdalrhman Hamida <abdalrhman7mida@gmail.com>
2022-12-17 16:47:26 +0200

    doc/doc/45/documentation: Doc  `DetoxPeriodAndGroupWithWhitelistedApps`

--- app/src/main/java/io/github/mohamed/sallam/awb/db/relationship/DetoxPeriodAndGroupWithWhitelistedApps.java
+++ app/src/main/java/io/github/mohamed/sallam/awb/db/relationship/DetoxPeriodAndGroupWithWhitelistedApps.java
@@ -7,6 +7,14 @@ import java.util.List;
 
 import io.github.mohamed.sallam.awb.db.entity.WhitelistedApp;
 
+/**
+ * `DetoxPeriodAndGroupWithWhitelistedApps` class is a relationship between three
+ * entities `DetoxPeriod`, `Group` and `WhitelistedApps` in order to query a detox
+ * period with the corresponding group including its whitelisted applications
+ * from database by using DAO method. It's one-to-many relationship.
+ *
+ * Source: https://developer.android.com/training/data-storage/room/relationships
+ */
 public class DetoxPeriodAndGroupWithWhitelistedApps {
     @Embedded
     public DetoxPeriodAndGroup detoxPeriodAndGroup;
