df491c255ae44771f50e39910080ae6eee634029
MohamedYehia20m <medoyehia2001@gmail.com>
2022-12-17 16:23:56 +0200

    doc/doc/45/documentation: documenting converter package
    - [x] converter package documentation

--- app/src/main/java/io/github/mohamed/sallam/awb/db/converter/TimestampConverter.java
+++ app/src/main/java/io/github/mohamed/sallam/awb/db/converter/TimestampConverter.java
@@ -5,16 +5,28 @@ import androidx.room.TypeConverter;
 import java.sql.Timestamp;
 
 /**
- * Converter Class to convert time from Timestamp to milliseconds (Long).
+ * Converter Class to convert time from Timestamp to milliseconds (Long) and vice versa.
+ *
+ * @see
  *
  * @author Mohamed Sherif
  */
 public class TimestampConverter {
+    /**
+     * @param milliSeconds represent time in milliseconds.
+     *
+     * @return time in Timestamp format.
+     */
     @TypeConverter
     public static Timestamp toTimestamp(Long milliSeconds){
         return (milliSeconds == null) ? null: new Timestamp(milliSeconds);
     }
 
+    /**
+     * @param timestamp represent time in Timestamp format.
+     *
+     * @return time in milliseconds format.
+     */
     @TypeConverter
     public static Long fromTimestamp(Timestamp timestamp){
         return (timestamp == null) ? null: timestamp.getTime();
--- app/src/main/java/io/github/mohamed/sallam/awb/db/converter/UuidConverter.java
+++ app/src/main/java/io/github/mohamed/sallam/awb/db/converter/UuidConverter.java
@@ -7,14 +7,26 @@ import java.util.UUID;
 /**
  * UUID converter class to use it with Room Data classes.
  *
+ * @see
+ *
  * @author Mohamed Sallam
  */
 public class UuidConverter {
+    /**
+     * @param uuid represent a group/device uuid.
+     *
+     * @return uuid but in a string format.
+     */
     @TypeConverter
     public static String fromUuid(UUID uuid) {
         return uuid.toString();
     }
 
+    /**
+     * @param string represent the string format of a group/device uuid.
+     *
+     * @return group/device uuid in UUID format.
+     */
     @TypeConverter
     public static UUID uuidFromString(String string) {
         return UUID.fromString(string);
