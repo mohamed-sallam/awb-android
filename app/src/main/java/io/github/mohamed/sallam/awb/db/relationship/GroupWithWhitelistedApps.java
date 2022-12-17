925357e14779146d4a94313fb19373978d98b9d1
Abdalrhman Hamida <abdalrhman7mida@gmail.com>
2022-12-17 16:46:29 +0200

    doc/doc/45/documentation: Doc `GroupWithWhitelistedApps`

--- app/src/main/java/io/github/mohamed/sallam/awb/db/relationship/GroupWithWhitelistedApps.java
+++ app/src/main/java/io/github/mohamed/sallam/awb/db/relationship/GroupWithWhitelistedApps.java
@@ -8,6 +8,13 @@ import java.util.List;
 import io.github.mohamed.sallam.awb.db.entity.WhitelistedApp;
 import io.github.mohamed.sallam.awb.db.entity.Group;
 
+/**
+ * `GroupWithWhitelistedApps` class is a relationship between two entities `Group`
+ * and `WhitelistedApp` in order to query a group with its applications
+ * from database by using DAO method.
+ *
+ * Source: https://developer.android.com/training/data-storage/room/relationships
+ */
 public class GroupWithWhitelistedApps {
     @Embedded
     public Group group;
