996fe462c159470aaf11a88a5e1fb2d451549a81
Abdalrhman Hamida <abdalrhman7mida@gmail.com>
2022-12-17 16:46:59 +0200

    doc/doc/45/documentation: Doc DeviceWithGroups

--- app/src/main/java/io/github/mohamed/sallam/awb/db/relationship/DeviceWithGroups.java
+++ app/src/main/java/io/github/mohamed/sallam/awb/db/relationship/DeviceWithGroups.java
@@ -8,6 +8,13 @@ import java.util.List;
 import io.github.mohamed.sallam.awb.db.entity.Device;
 import io.github.mohamed.sallam.awb.db.entity.Group;
 
+/**
+ * `DeviceWithGroups` class is a relationship between two entities `Device`
+ * and `Group` in order to query a Device with its internal groups
+ * from database by using DAO method. It's one-to-many relationship.
+ *
+ * Source: https://developer.android.com/training/data-storage/room/relationships
+ */
 public class DeviceWithGroups {
     @Embedded
     public Device device;
