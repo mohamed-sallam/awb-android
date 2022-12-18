82345d59f4777ff798ea7d90056d422c737f3cf1
MohamedYehia20m <medoyehia2001@gmail.com>
2022-12-18 16:58:53 +0200

    doc/doc/45/documentation: documenting DAOs
    - [x] documenting DetoxPeriodDao
    - [x] documenting DeviceDao
    - [ ] documenting GroupDao
    - [ ] documenting WhitelistedAppDao

--- app/src/main/java/io/github/mohamed/sallam/awb/db/dao/DeviceDao.java
+++ app/src/main/java/io/github/mohamed/sallam/awb/db/dao/DeviceDao.java
@@ -29,16 +29,37 @@ public interface DeviceDao {
     @Update
     void update(Device device);
 
-
+    /**
+     * a function that gets the uuid of a device to remove it from the devices table.
+     *
+     * @param deviceUuid represent the uuid of a device to be removed.
+     */
     @Query("DELETE FROM devices_table WHERE uuid=:deviceUuid")
     void delete(UUID deviceUuid);
 
+    /**
+     * a function that set a new uuid for a device and update the devices table.
+     *
+     * @param oldUuid represent the device uuid before update.
+     *
+     * @param newUuid represent the device uuid after update.
+     */
     @Query("UPDATE devices_table SET uuid=:newUuid WHERE uuid=:oldUuid")
     void setUuid(UUID oldUuid, UUID newUuid);
 
+    /**
+     * a function that gets all devices from the devices table.
+     *
+     * @return a live data of the list of devices from devices table.
+     */
     @Query("SELECT * FROM devices_table")
     LiveData<List<Device>> getAll();
 
+    /**
+     * a function that gets all devices with its groups from the devices table.
+     *
+     * @return a live data of the list of devices with groups from devices table.
+     */
     @Transaction
     @Query("SELECT * FROM devices_table")
     LiveData<List<DeviceWithGroups>> getAllWithGroups();
