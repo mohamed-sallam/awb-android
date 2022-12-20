21ae490afb416850a1f9e6ad5014a99bd6c2be9d
youssef-ahmmed <115221628+youssef-ahmmed@users.noreply.github.com>
2022-12-20 05:15:38 +0200

    frontend/refactor/52/edit_groupsadapter_v2: impl context menu & on click
    - [x] Add selector to group item
    - [x] Add colors for selection
    - [x] Add ids for options in context menu
    - [x] Add names on string resources
    - [x] Edit `view` file to `screen`

--- app/src/main/java/io/github/mohamed/sallam/awb/screen/adapter/GroupAdapter.java
+++ app/src/main/java/io/github/mohamed/sallam/awb/screen/adapter/GroupAdapter.java
@@ -1,15 +1,21 @@
 package io.github.mohamed.sallam.awb.screen.adapter;
 
-import android.graphics.Color;
+import android.annotation.SuppressLint;
+import android.view.ContextMenu;
 import android.view.LayoutInflater;
+import android.view.Menu;
 import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
+import androidx.core.content.ContextCompat;
 import androidx.recyclerview.widget.DiffUtil;
 import androidx.recyclerview.widget.ListAdapter;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.UUID;
+
+import io.github.mohamed.sallam.awb.R;
 import io.github.mohamed.sallam.awb.db.entity.Group;
 import io.github.mohamed.sallam.awb.databinding.GroupItemBinding;
 
@@ -19,16 +25,26 @@ import io.github.mohamed.sallam.awb.databinding.GroupItemBinding;
  * @author Abdurrahman Salah
  */
 public class GroupAdapter extends ListAdapter<Group, GroupAdapter.GroupViewHolder> {
+    private UUID selectedGroupUuid;
+    private int selectedPosition = -1;
+    private UUID longClickedGroupUuid;
 
-    private final OnLongPressListener onLongPressListener;
-    private final OnClickListener onClickListener;
-    //private int selectedPosition = 0;
-
-    public GroupAdapter(OnLongPressListener onLongPressListener,
-                           OnClickListener onClickListener) {
+    public GroupAdapter() {
         super(new Differ());
-        this.onClickListener = onClickListener;
-        this.onLongPressListener = onLongPressListener;
+    }
+
+    public UUID getLongClickedGroupUuid() {
+        return longClickedGroupUuid;
+    }
+
+    public UUID getSelectedGroupUuid() {
+        return selectedGroupUuid;
+    }
+
+    @Override
+    public void onViewRecycled(@NonNull GroupViewHolder holder) {
+        holder.itemView.setOnLongClickListener(null);
+        super.onViewRecycled(holder);
     }
 
     @NonNull
@@ -38,59 +54,70 @@ public class GroupAdapter extends ListAdapter<Group, GroupAdapter.GroupViewHolde
         return new GroupViewHolder(GroupItemBinding
                                    .inflate(LayoutInflater
                                             .from(parent.getContext()), parent
-                                            , false)
-                                            , onLongPressListener
-                                            , onClickListener);
+                                            , false));
     }
 
+    /**
+     * Source: https://stackoverflow.com/a/51080712/20414197
+     *
+     * @author Yousef Ahmed
+     * @author Mohamed Sallam
+     */
+    @SuppressLint("ResourceType")
     @Override
     public void onBindViewHolder(@NonNull GroupAdapter.GroupViewHolder holder,
                                  int position) {
-        Group item = getItem(position);
-        holder.bind(item);
-        holder.validateBackgroundColor(item);
+        Group group = getItem(position);
+        holder.bind(group);
+        holder.itemView.setSelected(selectedPosition == position);
+        holder.binding.groupNameEditText.setTextColor(ContextCompat
+                .getColor(holder.binding.groupNameEditText.getContext()
+                        ,R.color.white));
+        holder.itemView.setOnClickListener(v -> {
+            if (selectedPosition >= 0)
+                notifyItemChanged(selectedPosition);
+            selectedPosition = holder.getBindingAdapterPosition();
+            notifyItemChanged(selectedPosition);
+            selectedGroupUuid = holder.binding.getGroup().uuid;
+        });
+        holder.itemView.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                longClickedGroupUuid = holder.binding.getGroup().uuid;
+                return false;
+            }
+        });
     }
 
-    static class GroupViewHolder extends RecyclerView.ViewHolder {
-        GroupItemBinding binding;
+    public static class GroupViewHolder extends RecyclerView.ViewHolder
+                        implements View.OnCreateContextMenuListener {
+        public GroupItemBinding binding;
 
-        public GroupViewHolder(GroupItemBinding views,
-                               OnLongPressListener onLongPressListener,
-                               OnClickListener onClickListener) {
+        public GroupViewHolder(GroupItemBinding views) {
             super(views.getRoot());
             this.binding = views;
-            this.binding.groupItem.setOnLongClickListener(new View.OnLongClickListener() {
-                @Override
-                public boolean onLongClick(View v) {
-                    onLongPressListener.onLongPress(binding.getGroup());
-                    return false;
-                }
-            });
-            this.binding.groupItem.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    onClickListener.onClick(binding.getGroup());
-                    validateBackgroundColor(binding.getGroup());
-                }
-            });
+            // TODO: possible error
+            views.getRoot().setOnCreateContextMenuListener(this);
         }
 
-        void bind(Group group) {
+        public void bind(Group group) {
             binding.setGroup(group);
         }
 
-        public void validateBackgroundColor(Group item) {
-            if (item.isSelected()) {
-                binding.groupItem.setBackgroundColor(Color.GREEN);
-                binding.getGroup().setSelected(false);
-            } else {
-                binding.groupItem.setBackgroundColor(Color.DKGRAY);
-                binding.getGroup().setSelected(true);
-            }
+        @Override
+        public void onCreateContextMenu(ContextMenu contextMenu, View view, ContextMenu.ContextMenuInfo contextMenuInfo) {
+            contextMenu.add(Menu.NONE, R.id.renameGroupOption,
+                         0, R.string.rename);
+            contextMenu.add(Menu.NONE, R.id.duplicateGroupOption,
+                         1, R.string.duplicate);
+            contextMenu.add(Menu.NONE, R.id.editGroupOption,
+                         2, R.string.edit);
+            contextMenu.add(Menu.NONE, R.id.renameGroupOption,
+                         3, R.string.remove);
         }
     }
 
-    static class Differ extends DiffUtil.ItemCallback<Group> {
+    public static class Differ extends DiffUtil.ItemCallback<Group> {
 
         @Override
         public boolean areItemsTheSame(@NonNull Group oldItem, @NonNull Group newItem) {
@@ -102,14 +129,5 @@ public class GroupAdapter extends ListAdapter<Group, GroupAdapter.GroupViewHolde
             return oldItem.equals(newItem);
         }
     }
-
-    public interface OnLongPressListener {
-        void onLongPress(Group group);
-    }
-
-    public interface OnClickListener {
-        void onClick(Group group);
-    }
-
 }
 
--- app/src/main/java/io/github/mohamed/sallam/awb/screen/home/HomeFragment.java
+++ app/src/main/java/io/github/mohamed/sallam/awb/screen/home/HomeFragment.java
@@ -1,9 +1,11 @@
 package io.github.mohamed.sallam.awb.screen.home;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.NumberPicker;
@@ -15,10 +17,12 @@ import androidx.lifecycle.LifecycleOwner;
 import androidx.lifecycle.Observer;
 
 import java.util.Objects;
+import java.util.UUID;
 
 import io.github.mohamed.sallam.awb.R;
 import io.github.mohamed.sallam.awb.databinding.FragmentHomeBinding;
 import io.github.mohamed.sallam.awb.db.entity.Device;
+import io.github.mohamed.sallam.awb.screen.adapter.GroupAdapter;
 
 public class HomeFragment extends Fragment implements AddGroupDialog.GroupNameDialogListener {
 
@@ -26,6 +30,8 @@ public class HomeFragment extends Fragment implements AddGroupDialog.GroupNameDi
     private Device thisDevice;
     private HomeViewModel viewModel;
     private long duration;
+    private GroupAdapter groupAdapter;
+
     /**
      * Initialize the contents of the Activity's standard options menu.
      *
@@ -75,7 +81,7 @@ public class HomeFragment extends Fragment implements AddGroupDialog.GroupNameDi
                 openDialog();
             }
         });
-        viewModel.getThisDevice().observe((LifecycleOwner) this, new Observer<Device>() {
+        viewModel.getThisDevice().observe(getViewLifecycleOwner(), new Observer<Device>() {
             @Override
             public void onChanged(Device device) {
                 if (device == null) {
@@ -85,6 +91,16 @@ public class HomeFragment extends Fragment implements AddGroupDialog.GroupNameDi
                 }
             }
         });
+        groupAdapter = new GroupAdapter();
+        binding.recyclerView.setAdapter(groupAdapter);
+        registerForContextMenu(binding.recyclerView);
+        viewModel.getAllDevicesWithGroups().observe(getViewLifecycleOwner(), new Observer<List<DeviceWithGroups>>() {
+            @Override
+            public void onChanged(List<DeviceWithGroups> deviceWithGroups) {
+                groupAdapter.submitList(deviceWithGroups.get(0).groups);
+            }
+        });
+
         return binding.getRoot();
     }
 
@@ -92,6 +108,29 @@ public class HomeFragment extends Fragment implements AddGroupDialog.GroupNameDi
         AddGroupDialog addGroupDialog = new AddGroupDialog();
         addGroupDialog.setListener(this);
         addGroupDialog.show(getChildFragmentManager(), "Dialog");
+    @SuppressLint("NonConstantResourceId")
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        UUID groupUuid;
+        try {
+            groupUuid = groupAdapter.getLongClickedGroupUuid();
+        } catch (Exception e) {
+            return super.onContextItemSelected(item);
+        }
+        switch (item.getItemId()) {
+            case R.id.renameGroupOption:
+                break;
+            case R.id.duplicateGroupOption:
+
+                break;
+            case R.id.editGroupOption:
+
+                break;
+            case R.id.removeGroupOption:
+                viewModel.deleteGroup(groupUuid);
+                break;
+        }
+        return super.onContextItemSelected(item);
     }
 
     @Override
