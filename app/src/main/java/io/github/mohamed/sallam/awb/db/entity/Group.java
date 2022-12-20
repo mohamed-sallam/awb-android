f2f6610c66ae6b203628dce43962436fcff816bc
youssef-ahmmed <115221628+youssef-ahmmed@users.noreply.github.com>
2022-12-20 05:21:16 +0200

    frontend/refactor/52/edit_groupsadapter_v2: optimize the group entity class

--- app/src/main/java/io/github/mohamed/sallam/awb/db/entity/Group.java
+++ app/src/main/java/io/github/mohamed/sallam/awb/db/entity/Group.java
@@ -1,10 +1,10 @@
 package io.github.mohamed.sallam.awb.db.entity;
+
 import androidx.annotation.NonNull;
 import androidx.room.Entity;
 import androidx.room.PrimaryKey;
 import androidx.room.TypeConverters;
 
-import java.util.Objects;
 import java.util.UUID;
 
 import io.github.mohamed.sallam.awb.db.converter.UuidConverter;
@@ -25,35 +25,15 @@ public class Group implements IAggregateRoot {
 	public UUID uuid = UUID.randomUUID();
 	public String name;
 	public UUID deviceUuid;
-	private boolean isSelected;
-
-	public boolean isSelected() {
-		return isSelected;
-	}
-
-	public void setSelected(boolean selected) {
-		isSelected = selected;
-	}
-
-
-	public Group(String name) {
-		this.name = name;
-		this.isSelected = false;
-	}
-
-	public String getName() {
-		return name;
-	}
 
 	@Override
 	public boolean equals(Object o) {
 		if (o instanceof Group) {
 			Group group = (Group) o;
-			return Objects.equals(name, group.name)
-					&& Objects.equals(uuid, group.uuid)
-					&& Objects.equals(deviceUuid, group.deviceUuid);
+			return uuid.equals(group.uuid) &&
+				   name.equals(group.name) &&
+				   deviceUuid.equals(group.deviceUuid);
 		}
 		return false;
 	}
-
 }
