37a705e5a065207427694df23b75e15627e2ade8
Abdalrhman Hamida <abdalrhman7mida@gmail.com>
2022-12-21 23:50:22 +0200

    doc/doc/45/documentation: Doc `UpdateGroupViewModel`
    - [x] Doc for the class.
    - [x] Doc for the constructor.

--- app/src/main/java/io/github/mohamed/sallam/awb/view/updategroup/UpdateGroupViewModel.java
+++ app/src/main/java/io/github/mohamed/sallam/awb/view/updategroup/UpdateGroupViewModel.java
@@ -16,9 +16,11 @@ import io.github.mohamed.sallam.awb.db.relationship.GroupWithWhitelistedApps;
 import io.github.mohamed.sallam.awb.repo.GroupRepository;
 
 /**
- * UpdateGroupFragment ViewModel. It uses Command Pattern to queue applications
- * blocking and whitelisting til user save these command by clicking <save> or
- * discard them by clicking <cancel>.
+ * Update group ViewModel is responsible for preparing, holding and managing
+ * the data used in update group fragment.
+ * ViewModel uses Command Pattern to queue applications blocking and whitelisting
+ * till user saves these command by clicking <save> or discard them by clicking
+ * <cancel>.
  *
  * @see <a href =”https://refactoring.guru/design-patterns/command”>Command</a>
  *
@@ -29,6 +31,15 @@ public class UpdateGroupViewModel extends AndroidViewModel {
     private final Map<String, AppCommand> appCommands;
     private final UUID groupUuid;
 
+    /**
+     * Instantiation of required repositories in order to access the
+     * required methods to hande the data used in `UpdateGroupFragment`.
+     *
+     * @param application is the context where the `Application` class in Android
+     * is the base class within an Android app that contains all other.
+     * @param groupUuid unique identifier for the group we are dealing with.
+     *
+     */
     public UpdateGroupViewModel(@NonNull Application application, UUID groupUuid) {
         super(application);
         groupRepository = new GroupRepository(application);
