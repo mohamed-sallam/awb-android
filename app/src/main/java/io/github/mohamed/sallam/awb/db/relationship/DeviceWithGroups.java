d2c5d8b57477f33825235cbbd5ad1cc04decc85c
youssef-ahmmed <115221628+youssef-ahmmed@users.noreply.github.com>
2022-12-26 19:29:46 +0200

    frontend/feat/43/impl_homefragment_v2: remove unneeded `equals()`

--- app/src/main/java/io/github/mohamed/sallam/awb/db/relationship/DeviceWithGroups.java
+++ app/src/main/java/io/github/mohamed/sallam/awb/db/relationship/DeviceWithGroups.java
@@ -1,11 +1,9 @@
 package io.github.mohamed.sallam.awb.db.relationship;
 
-import androidx.annotation.Nullable;
 import androidx.room.Embedded;
 import androidx.room.Relation;
 
 import java.util.List;
-import java.util.Objects;
 
 import io.github.mohamed.sallam.awb.db.entity.Device;
 import io.github.mohamed.sallam.awb.db.entity.Group;
@@ -18,10 +16,4 @@ public class DeviceWithGroups {
             entityColumn = "deviceUuid"
     )
     public List<Group> groups;
-
-    // TODO write equal method to check if the device and it's group are the same.
-    @Override
-    public boolean equals(@Nullable Object obj) {
-        return super.equals(obj);
-    }
 }
