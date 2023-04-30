267c2594e4d2228ba79666258c122dfd6b72476c
Mohd-Sherif <Mohd-Sherif@outlook.com>
2023-05-01 00:42:17 +0300

    backend/test/65/implement_junit_testing_for_instrumental_n_local: `UserDatabaseTest` Class

--- app/src/androidTest/java/io/github/mohamed/sallam/awb/db/UserDataBaseTest.java
+++ app/src/androidTest/java/io/github/mohamed/sallam/awb/db/UserDataBaseTest.java
@@ -1,4 +1,37 @@
 package io.github.mohamed.sallam.awb.db;
 
+import androidx.room.Room;
+import androidx.test.core.app.ApplicationProvider;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.filters.SmallTest;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.runner.RunWith;
+
+import io.github.mohamed.sallam.awb.db.dao.DeviceDao;
+
+@RunWith(AndroidJUnit4.class)
+@SmallTest
 public class UserDatabaseTest {
+
+    private UserDatabase userDatabase;
+
+    @Before
+    public void setup(){
+        userDatabase = Room.inMemoryDatabaseBuilder(
+                ApplicationProvider.getApplicationContext(),
+                UserDatabase.class
+        ).build();
+    }
+
+    @After
+    public void tearDown(){
+        userDatabase.close();
+    }
+
+    public DeviceDao getDeviceDao(){
+
+    }
+
 }
