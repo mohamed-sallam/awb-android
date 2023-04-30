3a8e57e18cec583df35b75a8f9c222eb380ded3e
Mohd-Sherif <Mohd-Sherif@outlook.com>
2023-05-01 01:02:59 +0300

    backend/test/65/implement_junit_testing_for_instrumental_n_local: Adding Imports

--- app/src/androidTest/java/io/github/mohamed/sallam/awb/db/dao/DeviceDaoTest.java
+++ app/src/androidTest/java/io/github/mohamed/sallam/awb/db/dao/DeviceDaoTest.java
@@ -1,4 +1,22 @@
 package io.github.mohamed.sallam.awb.db.dao;
 
 public class DeviceDaoTest {
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule;
+import androidx.lifecycle.LiveData;
+import androidx.test.filters.SmallTest;
+import androidx.test.runner.AndroidJUnit4;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import io.github.mohamed.sallam.awb.db.UserDatabaseTest;
+import io.github.mohamed.sallam.awb.db.entity.Device;
 }
