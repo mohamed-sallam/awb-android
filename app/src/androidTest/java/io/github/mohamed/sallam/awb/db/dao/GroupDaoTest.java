38592a1e3961ff4a5a161dcd6a57aa8a2188a507
MohamedYehia20m <77248801+MohamedYehia20m@users.noreply.github.com>
2023-05-01 01:08:08 +0200

    backend/test/65/implement_junit_testing_for_instrumental_n_local: establising GroupDaoTest

--- app/src/androidTest/java/io/github/mohamed/sallam/awb/db/dao/GroupDaoTest.java
+++ app/src/androidTest/java/io/github/mohamed/sallam/awb/db/dao/GroupDaoTest.java
@@ -1,4 +1,91 @@
 package io.github.mohamed.sallam.awb.db.dao;
 
+
+import android.app.Application;
+
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.filters.SmallTest;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import io.github.mohamed.sallam.awb.db.UserDatabase;
+import kotlinx.coroutines.ExperimentalCoroutinesApi;
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule;
+
+
+
+@ExperimentalCoroutinesApi
+@RunWith(AndroidJUnit4::class) //search for a better annotation //search for its purpose!
+@SmallTest
+
 public class GroupDaoTest {
+
+    @Rule
+    public InstantTaskExecutorRule instantTaskExecutorRule = new InstantTaskExecutorRule();
+
+    private GroupDao groupDao ; //final??
+    UserDatabase db ; //database instance ??
+
+
+
+
+
+    @Before
+    public void setup(Application application) {
+        db = UserDatabase.getInstance(application);
+        groupDao = db.groupDao(); //setup //why is
+
+    }
+
+
+
+    @After
+    public void teardown() {
+        db.close();
+    }
+
+/*
+    @BeforeClass
+
+    @AfterClass
+
+*/
+
+
+    @Test
+    public void testInsert() {
+    }
+
+    @Test
+    public void testRename() {
+    }
+
+    @Test
+    public void testDelete() {
+    }
+
+    @Test
+    public void testGet() {
+    }
+
+    @Test
+    public void testGetAllByDevice() {
+    }
+
+    @Test
+    public void testReplaceDeviceUuid() {
+    }
+
+    @Test
+    public void testGetAllForThisDevice() {
+    }
+
+
 }
