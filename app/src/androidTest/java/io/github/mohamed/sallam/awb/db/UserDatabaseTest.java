ee721f8accb755003f2fa4fec342105d7752048b
youssef-ahmmed <115221628+youssef-ahmmed@users.noreply.github.com>
2023-05-02 20:54:17 +0200

    backend/test/65/implement_junit_testing_for_instrumental_n_local: update database test file
    - Remove noisy comments.
    - Remove `DeviceDao` method.

--- app/src/androidTest/java/io/github/mohamed/sallam/awb/db/UserDatabaseTest.java
+++ app/src/androidTest/java/io/github/mohamed/sallam/awb/db/UserDatabaseTest.java
@@ -9,25 +9,12 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.runner.RunWith;
 
-import io.github.mohamed.sallam.awb.db.dao.DeviceDao;
 
-/**
- * Testing our internal database.
- *
- * @author Mohamed Sherif
- */
 @RunWith(AndroidJUnit4.class)
 @SmallTest
 public class UserDatabaseTest {
-
     private UserDatabase userDatabase;
 
-    /**
-     * Making a new instance of the database before every test case.
-     *
-     * Providing that this instance is created in the ram that
-     * to be easy for testing.
-     */
     @Before
     public void setup(){
         userDatabase = Room.inMemoryDatabaseBuilder(
@@ -36,16 +23,8 @@ public class UserDatabaseTest {
         ).build();
     }
 
-    /**
-     * Closing the instance with the end of every test case.
-     */
     @After
     public void tearDown(){
         userDatabase.close();
     }
-
-    public DeviceDao getDeviceDao(){
-
-    }
-
 }
