16b5470f612ccc4b9ec20ffd187ccb02c18fe6d2
Abdalrhman Hamida <96482182+Abdalrhman7mida@users.noreply.github.com>
2023-05-04 01:06:18 +0300

    backend/test/65/implement_junit_testing_for_instrumental_n_local: `DeviceRepository` is tested
    - [x] Using `MainThreadExecutorService` class on constructor to handle Threads.
    - [x] Init `deviceRepository` variable correctly.
    - [x] Done testing for all methods of `DeviceRepository`.
    - [x] Rename the test methods for convention purposes.

--- app/src/test/java/io/github/mohamed/sallam/awb/repo/DeviceRepositoryTest.java
+++ app/src/test/java/io/github/mohamed/sallam/awb/repo/DeviceRepositoryTest.java
@@ -1,10 +1,11 @@
 package io.github.mohamed.sallam.awb.repo;
 
 import static org.junit.Assert.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.verifyNoMoreInteractions;
-import static org.mockito.Mockito.when;
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule;
 import androidx.lifecycle.LiveData;
@@ -20,10 +21,12 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
+import io.github.mohamed.sallam.awb.db.UserDatabase;
 import io.github.mohamed.sallam.awb.db.dao.GroupDao;
 import io.github.mohamed.sallam.awb.db.dao.DeviceDao;
 import io.github.mohamed.sallam.awb.db.entity.Device;
 import io.github.mohamed.sallam.awb.util.LiveDataTestUtil;
+import io.github.mohamed.sallam.awb.util.MainThreadExecutorService;
 import io.github.mohamed.sallam.awb.util.TestUtil;
 
 @RunWith(MockitoJUnitRunner.class)
@@ -36,13 +39,16 @@ public class DeviceRepositoryTest {
 
     @Before
     public void setUp() {
+        UserDatabase.databaseWriteExecutor = new MainThreadExecutorService();
         groupDao = mock(GroupDao.class);
         deviceDao = mock(DeviceDao.class);
-        deviceRepository = new DeviceRepository(groupDao, deviceDao);
+        LiveData<List<Device>> liveDataDevices = new MutableLiveData<>(TestUtil.TEST_DEVICE_LIST);
+        deviceRepository = new DeviceRepository(groupDao, deviceDao,
+                                                TestUtil.TEST_DEVICE_1, liveDataDevices);
     }
 
     @Test
-    public void testInsert_DaoInsertCalled() {
+    public void testInsert() {
         // Arrange
         Device device = TestUtil.TEST_DEVICE_1;
 
@@ -55,7 +61,7 @@ public class DeviceRepositoryTest {
     }
 
     @Test
-    public void testUpdate_DaoUpdateCalled() {
+    public void testUpdate() {
         // Arrange
         Device device = TestUtil.TEST_DEVICE_1;
 
@@ -68,40 +74,39 @@ public class DeviceRepositoryTest {
     }
 
     @Test
-    public void testDelete_daoDeleteCalled() {
+    public void testDelete() {
         // Arrange
-        Device device = TestUtil.TEST_DEVICE_1;
+        UUID deviceUuid = TestUtil.TEST_UUID_3;
 
         // Act
-        deviceRepository.delete(device.uuid);
+        deviceRepository.delete(deviceUuid);
 
         // Assert
-        verify(deviceDao).delete(device.uuid);
+        verify(deviceDao).delete(deviceUuid);
         verifyNoMoreInteractions(deviceDao);
     }
 
     @Test
-    public void testGenerateUuid_DaoMethodsCalled() {
+    public void testGenerateUuid() {
         // Arrange
         UUID oldDeviceUuid = TestUtil.TEST_UUID_1;
-        UUID newDeviceUuid = TestUtil.TEST_UUID_3;
 
         // Act
         deviceRepository.generateUuid(oldDeviceUuid);
 
         // Assert
-        verify(groupDao).replaceDeviceUuid(oldDeviceUuid, newDeviceUuid);
-        verify(deviceDao).setUuid(oldDeviceUuid, newDeviceUuid);
+        verify(groupDao).replaceDeviceUuid(eq(oldDeviceUuid), any(UUID.class));
+        verify(deviceDao).setUuid(eq(oldDeviceUuid), any(UUID.class));
         verifyNoMoreInteractions(groupDao, deviceDao);
     }
 
     @Test
-    public void testGetAll_ReturnsLiveDataDevices() throws InterruptedException {
+    public void testGetAll() throws InterruptedException {
         // Arrange
         List<Device> deviceList = new ArrayList<>();
         deviceList.add(TestUtil.TEST_DEVICE_1);
+        deviceList.add(TestUtil.TEST_DEVICE_2);
         LiveData<List<Device>> liveDataDevices = new MutableLiveData<>(deviceList);
-        when(deviceDao.getAll()).thenReturn(liveDataDevices);
 
         // Act
         List<Device> result = new LiveDataTestUtil<List<Device>>().getValue(
@@ -113,16 +118,17 @@ public class DeviceRepositoryTest {
     }
 
     @Test
-    public void testGetThisDevice_ReturnsLiveDataDevice() {
+    public void testGetThisDevice() throws InterruptedException {
         // Arrange
         Device device = TestUtil.TEST_DEVICE_1;
         LiveData<Device> liveDataDevice = new MutableLiveData<>(device);
-        when(deviceDao.getThisDevice()).thenReturn(liveDataDevice);
 
         // Act
-        LiveData<Device> result = deviceRepository.getThisDevice();
+        Device result = new LiveDataTestUtil<Device>().getValue(
+                deviceRepository.getThisDevice()
+        );
 
         // Assert
-        assertEquals(liveDataDevice, result);
+        assertEquals(liveDataDevice.getValue(), result);
     }
 }
