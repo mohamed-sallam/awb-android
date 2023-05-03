777ddefab9f087823019e89c51ce567f5c56ecfd
Abdalrhman Hamida <96482182+Abdalrhman7mida@users.noreply.github.com>
2023-05-04 01:02:05 +0300

    backend/test/65/implement_junit_testing_for_instrumental_n_local: Override `MainThreadExecutorService` execute method
    - This class is mainly created to override the method responsible for handling the number of thread created to handle processes running
    
    - Main purpose of the class to be used for testing purpose
        -  Overriding the `execute()` to indicate that any process or any running code or methods
           will  done on the main thread

---
+++ app/src/test-common/java/io/github/mohamed/sallam/awb/util/MainThreadExecutorService.java
@@ -0,0 +1,78 @@
+package io.github.mohamed.sallam.awb.util;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+
+public class MainThreadExecutorService implements ExecutorService {
+    @Override
+    public void shutdown() {
+
+    }
+
+    @Override
+    public List<Runnable> shutdownNow() {
+        return null;
+    }
+
+    @Override
+    public boolean isShutdown() {
+        return false;
+    }
+
+    @Override
+    public boolean isTerminated() {
+        return false;
+    }
+
+    @Override
+    public boolean awaitTermination(long l, TimeUnit timeUnit) throws InterruptedException {
+        return false;
+    }
+
+    @Override
+    public <T> Future<T> submit(Callable<T> callable) {
+        return null;
+    }
+
+    @Override
+    public <T> Future<T> submit(Runnable runnable, T t) {
+        return null;
+    }
+
+    @Override
+    public Future<?> submit(Runnable runnable) {
+        return null;
+    }
+
+    @Override
+    public <T> List<Future<T>> invokeAll(Collection<? extends Callable<T>> collection) throws InterruptedException {
+        return null;
+    }
+
+    @Override
+    public <T> List<Future<T>> invokeAll(Collection<? extends Callable<T>> collection, long l, TimeUnit timeUnit) throws InterruptedException {
+        return null;
+    }
+
+    @Override
+    public <T> T invokeAny(Collection<? extends Callable<T>> collection) throws ExecutionException, InterruptedException {
+        return null;
+    }
+
+    @Override
+    public <T> T invokeAny(Collection<? extends Callable<T>> collection, long l, TimeUnit timeUnit) throws ExecutionException, InterruptedException, TimeoutException {
+        return null;
+    }
+
+    @Override
+    public void execute(Runnable runnable) {
+        runnable.run();
+    }
+
+}
