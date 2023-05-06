9e282e83642c4756e105d1c6f8eb366471a7f5cb
Medhat Mohammed <medhatmohammed1612@gmail.com>
2023-05-06 17:33:16 +0300

    backend/test/65/implement_junit_testing_for_instrumental_n_local: setUp and basic methods testing in DetoxPeriodRepositoryTest class

--- app/src/test/java/io/github/mohamed/sallam/awb/repo/DetoxPeriodRepositoryTest.java
+++ app/src/test/java/io/github/mohamed/sallam/awb/repo/DetoxPeriodRepositoryTest.java
@@ -1,14 +1,18 @@
 package io.github.mohamed.sallam.awb.repo;
 
+import static org.mockito.Mockito.*;
+
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule;
 
 import org.junit.*;
-
 import org.junit.runner.RunWith;
 import org.mockito.junit.MockitoJUnitRunner;
 
+import io.github.mohamed.sallam.awb.db.UserDatabase;
 import io.github.mohamed.sallam.awb.db.dao.DetoxPeriodDao;
-
+import io.github.mohamed.sallam.awb.db.entity.DetoxPeriod;
+import io.github.mohamed.sallam.awb.util.MainThreadExecutorService;
+import io.github.mohamed.sallam.awb.util.TestUtil;
 
 @RunWith(MockitoJUnitRunner.class)
 public class DetoxPeriodRepositoryTest {
@@ -20,17 +24,45 @@ public class DetoxPeriodRepositoryTest {
 
     @Before
     public void setUp() {
+        UserDatabase.databaseWriteExecutor = new MainThreadExecutorService();
+        detoxPeriodDao = mock(DetoxPeriodDao.class);
+        detoxPeriodRepository = new DetoxPeriodRepository(detoxPeriodDao);
     }
 
     @Test
     public void testInsert() {
+        // Arrange
+        DetoxPeriod detoxPeriod = TestUtil.TEST_DETOX_PERIOD_1;
+
+        // Act
+        detoxPeriodRepository.insert(detoxPeriod);
+
+
+        // Assert
+        verify(detoxPeriodDao).insert(detoxPeriod);
+        verifyNoMoreInteractions(detoxPeriodDao);
     }
 
     @Test
     public void testUpdate() {
+        // Arrange
+        DetoxPeriod detoxPeriod = TestUtil.TEST_DETOX_PERIOD_1;
+
+        // Act
+        detoxPeriodRepository.update(detoxPeriod);
+
+        // Assert
+        verify(detoxPeriodDao).update(detoxPeriod);
+        verifyNoMoreInteractions(detoxPeriodDao);
     }
 
     @Test
     public void testDelete() {
+        // Act
+        detoxPeriodRepository.delete();
+
+        // Assert
+        verify(detoxPeriodDao).delete();
+        verifyNoMoreInteractions(detoxPeriodDao);
     }
 }
